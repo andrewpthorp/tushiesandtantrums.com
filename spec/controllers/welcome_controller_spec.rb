06515c77ce1b15d397c06c619b40007900003ddc
Andrew Thorp <andrewpthorp@gmail.com>
2013-07-23 14:58:07 -0500

    added sample /about page

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -9,4 +9,11 @@ describe WelcomeController do
     end
   end
 
+  describe 'GET about' do
+    it 'should respond with an HTTP 200' do
+      get :index
+      response.should be_success
+    end
+  end
+
 end
