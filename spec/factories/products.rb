64f560e9049c38f4a43bc2dbdc5cb0271a673649
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-05 14:31:42 -0500

    first pass at products#show

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :product do
     name            { Faker::Commerce.product_name }
-    description     { Faker::Lorem.paragraph(5) }
+    description     { Faker::Lorem.paragraphs(5).join("<br /><br />") }
     price_in_cents  { Faker::Number.number(3) }
     image           { File.open(Dir.glob(File.join(Rails.root, 'lib', 'assets', 'images', 'products', '*')).sample) }
     tag_list        { ['boys', 'girls'].sample }
