15dbae508032bfabee85095916174a5ad0a1f1ca
Andrew Thorp <andrewpthorp@gmail.com>
2013-09-08 17:39:28 -0500

    factory updates

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :product do
     name                { Faker::Commerce.product_name }
-    description         { Faker::Lorem.paragraphs(5).join("<br /><br />") }
+    description         { Faker::Lorem.paragraphs(5).join("\n\n") }
     price_in_cents      { rand(1000) * 100 }
     shipping_in_cents   { rand(50) * 100 }
     image               { File.open(Dir.glob(File.join(Rails.root, 'lib', 'assets', 'images', 'products', '*')).sample) }
